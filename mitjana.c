b1e35d5fc9eece096cbdf09ed9fd8b934a799386
justhanable <jonathan.bs91@gmail.com>
2019-05-05 19:46:56 +0200

    Segon commit del projecte mitjana

--- mitjana.c
+++ mitjana.c
@@ -16,6 +16,6 @@ int main() {
    scanf(" %c",&consulta);
    }
  mitjana=suma/numero_repeticions;
- printf("La mitjana resultant és: %.2f\n",mitjana);
+ printf(" La mitjana resultant és: %.2f\n",mitjana);
  return 0;
 }
